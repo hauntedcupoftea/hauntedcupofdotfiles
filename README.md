ed707539a90b36ec8deb5ac37798cece66db8cad
hauntedcupoftea <andydchauhan@gmail.com>
2025-02-08 15:02:47 +0530

    add basic layout for primary machine

--- README.md
+++ README.md
@@ -5,4 +5,7 @@ A repository containing nixOS configurations for all my funny little devices. Pe
 
 ## Running it for yourself
 
-Since I'm creating a flake, running this setup would be as simple as cloning, copying over your host configuration to a new folder and rebuilding flake with that host.
+Since I'm creating a flake, running this setup would be as simple as cloning, copying over your host configuration to a new folder and rebuilding flake with that host. (will update this with more clear instructions later).
+
+current convention's I'm following:
+- Home Manager imports \[user].nix instead of home.nix (to make multi-user management easier).
---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "Nixos config flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  } @ inputs: {
+    nixosConfigurations.default = nixpkgs.lib.nixosSystem {
+      specialArgs = {inherit inputs;};
+      modules = [
+        ./hosts/default/configuration.nix
+        inputs.home-manager.nixosModules.default
+      ];
+    };
+    homeConfigurations.homeConfigName = inputs.home-manager.lib.homeManagerConfiguration {
+      # Specify the host architecture
+      pkgs = nixpkgs.legacyPackages."x86_64-linux";
+
+      # Specify your home configuration modules here, for example,
+      # the path to your home.nix.
+      modules = [./home.nix];
+
+      extraSpecialArgs = {inherit inputs;};
+    };
+  };
+}
