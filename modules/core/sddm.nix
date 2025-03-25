b2e0ace7d4581d33f36166948841ab0739301b71
hauntedcupoftea <andydchauhan@gmail.com>
2025-03-25 20:17:48 +0530

    sddm qt6 version

--- modules/core/sddm.nix
+++ modules/core/sddm.nix
@@ -3,14 +3,15 @@
     enable = true;
     wayland.enable = true;
     theme = "catppuccin-mocha";
+    package = pkgs.kdePackages.sddm;
   };
 
   environment.systemPackages = [
     (
       pkgs.catppuccin-sddm.override {
         flavor = "mocha";
-        font = "Noto Sans";
-        fontSize = "9";
+        font = "FiraCode Nerd Font";
+        fontSize = "16";
         # background = "${./wallpaper.png}";
         # loginBackground = true;
       }
