629c063e36c3d928910cf125942ebed06abfdccd
hauntedcupoftea <andydchauhan@gmail.com>
2025-03-25 20:09:19 +0530

    polkit and keyring additions too

--- modules/core/security.nix
+++ modules/core/security.nix
@@ -2,4 +2,25 @@
   security.rtkit.enable = true;
   security.sudo.enable = true;
   security.pam.services.hyprlock = {};
+
+  # enable gnome-keyring because it just works(tm)
+  services.gnome.gnome-keyring.enable = true;
+  security.pam.services.hyprland.enableGnomeKeyring = true;
+
+  # gnome polkit auto-start from nix wiki
+  systemd = {
+    user.services.polkit-gnome-authentication-agent-1 = {
+      description = "polkit-gnome-authentication-agent-1";
+      wantedBy = ["graphical-session.target"];
+      wants = ["graphical-session.target"];
+      after = ["graphical-session.target"];
+      serviceConfig = {
+        Type = "simple";
+        ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+        Restart = "on-failure";
+        RestartSec = 1;
+        TimeoutStopSec = 10;
+      };
+    };
+  };
 }
