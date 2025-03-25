e26bd90f58ed228d744b15c6ab1888dfca977ddd
hauntedcupoftea <andydchauhan@gmail.com>
2025-03-25 18:26:59 +0530

    fix syntax error for sddm gnome

--- modules/core/security.nix
+++ modules/core/security.nix
@@ -2,8 +2,6 @@
   security.rtkit.enable = true;
   security.sudo.enable = true;
   # security.pam.services.hyprlock = {};
-  services.xserver.displayManager.sddm = {
-    enable = true;
-    wayland.enable = true;
-  };
+  services.xserver.displayManager.sddm.enable = true;
+  services.displayManager.sddm.wayland.enable = true;
 }
