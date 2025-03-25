b4701f02cc267344f7fe7bce32514761d1e2e154
hauntedcupoftea <andydchauhan@gmail.com>
2025-03-25 18:22:21 +0530

    disable gdm

--- modules/desktop/gnome.nix
+++ modules/desktop/gnome.nix
@@ -5,7 +5,6 @@
   ...
 }: {
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
 
   # Common GNOME utilities
