53b25330ec50e3f66cc4a41b0613deba840884cc
hauntedcupoftea <andydchauhan@gmail.com>
2025-04-01 15:20:26 +0530

    this seems to have fixed it

--- flake.nix
+++ flake.nix
@@ -17,7 +17,7 @@
 
     zen-browser = {
       url = "github:0xc000022070/zen-browser-flake";
-      inputs.nixpkgs.follows = "nixpkgs";
+      # inputs.nixpkgs.follows = "nixpkgs";
     };
 
     spicetify-nix = {
