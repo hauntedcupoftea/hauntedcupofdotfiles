39af61cf74fba9d14c22b8dd1972c97e20a59847
hauntedcupoftea <andydchauhan@gmail.com>
2025-05-27 17:47:00 +0530

    add docker daemon config to home manager

--- home/dev/docker.nix
+++ home/dev/docker.nix
@@ -1,6 +1,19 @@
 { pkgs, ... }: {
   programs.lazydocker.enable = true;
-  home.packages = [
-    pkgs.lazydocker
-  ];
+  home = {
+    packages = [
+      pkgs.lazydocker
+    ];
+    file = {
+      ".config/docker/daemon.json" = {
+        text = ''
+          {
+            "dns": ["1.1.1.1", "8.8.8.8"],
+            "ipv6": false,
+            "experimental": false
+          }
+        '';
+      };
+    };
+  };
 }
