9098a02645e583d73690d16811d2c0d9eab6cf1c
hauntedcupoftea <andydchauhan@gmail.com>
2025-05-27 17:03:14 +0530

    disable ipv6

--- modules/core/networking.nix
+++ modules/core/networking.nix
@@ -3,16 +3,16 @@
   # Enable networking
   networking = {
     networkmanager.enable = true;
-    enableIPv6 = true;
+    # enableIPv6 = true;
     # Configure network proxy if necessary
     # proxy.default = "http://user:password@proxy:port/";
     # proxy.noProxy = "127.0.0.1,localhost,internal.domain";
   };
 
-  boot.kernel.sysctl = {
-    "net.ipv6.conf.all.accept_ra" = 2;
-    "net.ipv6.conf.default.accept_ra" = 2;
-  };
+  # boot.kernel.sysctl = {
+  #   "net.ipv6.conf.all.accept_ra" = 2;
+  #   "net.ipv6.conf.default.accept_ra" = 2;
+  # };
 
   hardware.bluetooth = {
     enable = true;
