cfb5270b039ba1fc60524422400e7c8403ed581b
hauntedcupoftea <andydchauhan@gmail.com>
2025-05-27 17:01:04 +0530

    lmao ipv6 was missing on my router

--- modules/core/networking.nix
+++ modules/core/networking.nix
@@ -3,10 +3,15 @@
   # Enable networking
   networking = {
     networkmanager.enable = true;
-
+    enableIPv6 = true;
     # Configure network proxy if necessary
     # proxy.default = "http://user:password@proxy:port/";
     # proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+  };
+
+  boot.kernel.sysctl = {
+    "net.ipv6.conf.all.accept_ra" = 2;
+    "net.ipv6.conf.default.accept_ra" = 2;
   };
 
   hardware.bluetooth = {
