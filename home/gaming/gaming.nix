cb658451dad5fd56531d5ceaaa98fc1babd01141
hauntedcupoftea <andydchauhan@gmail.com>
2025-06-06 16:01:36 +0530

    add more wine compat stuff

--- home/gaming/gaming.nix
+++ home/gaming/gaming.nix
@@ -2,9 +2,12 @@
   home.packages = with pkgs; [
     mangohud
     protonup-qt
+    protonplus
     lutris
     bottles
     heroic
     dualsensectl
+    wineWowPackages.stable
+    winetricks
   ];
 }
