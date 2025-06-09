46cd13324e9a820a4fa11dd3f6822dff1326792c
hauntedcupoftea <andydchauhan@gmail.com>
2025-06-09 15:15:06 +0530

    latest kernel + geoclue 2

--- modules/core/location.nix
+++ modules/core/location.nix
@@ -1,7 +1,10 @@
 { pkgs, ... }: {
-  services.gpsd = {
+  services.geoclue2 = {
     enable = true;
   };
+
+  location.provider = "geoclue2";
+
   environment.systemPackages = [
     pkgs.gnome-maps
   ];
