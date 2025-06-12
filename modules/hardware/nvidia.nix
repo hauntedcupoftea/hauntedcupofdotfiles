54c4231e09eae2cb8cc182f23cf1d5dd91a7be6a
hauntedcupoftea <andydchauhan@gmail.com>
2025-06-13 01:46:57 +0530

    update + nvidia pwm enable

--- modules/hardware/nvidia.nix
+++ modules/hardware/nvidia.nix
@@ -1,8 +1,7 @@
-{
-  config,
-  lib,
-  pkgs,
-  ...
+{ config
+, lib
+, pkgs
+, ...
 }: {
   # Enable OpenGL
   hardware.graphics = {
@@ -10,14 +9,14 @@
   };
 
   # Load nvidia driver for Xorg and Wayland
-  services.xserver.videoDrivers = ["nvidia"];
+  services.xserver.videoDrivers = [ "nvidia" ];
 
   hardware.nvidia = {
     # Modesetting is required.
     modesetting.enable = true;
 
     # Power management settings
-    powerManagement.enable = false;
+    powerManagement.enable = true;
     powerManagement.finegrained = false;
 
     # Using proprietary driver
