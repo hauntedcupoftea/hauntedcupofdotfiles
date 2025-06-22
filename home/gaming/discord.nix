cd124b00ca2f659bdc3ec5b739bc9890df412137
hauntedcupoftea <andydchauhan@gmail.com>
2025-06-22 14:21:05 +0530

    maybe standard vesktop wrapper is smart enough?

--- home/gaming/discord.nix
+++ home/gaming/discord.nix
@@ -1,18 +1,6 @@
 { pkgs, ... }: {
   home.packages = with pkgs; [
-    (pkgs.vesktop.override {
-      commandLineArgs = [
-        # tells electron to use the VA-API for both decoding and encoding video.
-        "--enable-features=VaapiVideoDecoder,VaapiVideoEncoder"
-        # Forces hardware acceleration even if electron thinks your GPU is unsupported.
-        "--ignore-gpu-blocklist"
-        # General performance improvements for rendering.
-        "--enable-gpu-rasterization"
-        "--enable-zero-copy"
-        # ozone hint for wayland/hyprland.
-        "--ozone-platform-hint=auto"
-      ];
-    })
+    vesktop
     arrpc
   ];
 
