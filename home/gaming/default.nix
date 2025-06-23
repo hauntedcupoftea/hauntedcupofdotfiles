16d93586fe8cbd81001b4783177096d0c50fe604
hauntedcupoftea <andydchauhan@gmail.com>
2025-06-23 21:24:21 +0530

    samrewritten sucks lol.

--- home/gaming/default.nix
+++ home/gaming/default.nix
@@ -1,12 +1,7 @@
-{ pkgs, ... }: {
+{ ... }: {
   imports = [
     ./discord.nix
     ./gaming.nix
     ./minecraft.nix
-  ];
-
-  # MISC packages
-  home.packages = with pkgs; [
-    samrewritten # steam achievements manager
   ];
 }
