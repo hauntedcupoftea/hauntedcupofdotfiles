d1f013e5257a3e73b9c91eefc853952a0e3adeb3
hauntedcupoftea <andydchauhan@gmail.com>
2025-06-28 18:46:56 +0530

    disable suspend on hypridle (nvidia issues)

--- home/hyprland/hypridle.nix
+++ home/hyprland/hypridle.nix
@@ -29,10 +29,11 @@
           on-timeout = "hyprctl dispatch dpms off"; # screen off when timeout has passed
           on-resume = "hyprctl dispatch dpms on && brightnessctl -r"; # screen on when activity is detected after timeout has fired.
         }
-        {
-          timeout = 1800; # 30min
-          on-timeout = "systemctl suspend"; # suspend pc
-        }];
+        # {
+        #   timeout = 1800; # 30min
+        #   on-timeout = "systemctl suspend"; # suspend pc
+        # }
+      ];
     };
   };
 }
