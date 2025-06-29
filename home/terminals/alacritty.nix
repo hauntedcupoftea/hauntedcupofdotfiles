d5a8bded2d5d649aba9b52d2e1d7b29627736c2b
hauntedcupoftea <andydchauhan@gmail.com>
2025-06-29 15:06:04 +0530

    update config a bit

--- home/terminals/alacritty.nix
+++ home/terminals/alacritty.nix
@@ -1,5 +1,15 @@
 { ... }: {
   programs.alacritty = {
     enable = true;
+    settings = {
+      font = {
+        size = 12;
+      };
+      window = {
+        opacity = 0.8;
+        blue = true;
+      };
+      cursor.blinking = "On";
+    };
   };
 }
