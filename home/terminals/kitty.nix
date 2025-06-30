bb57a2ff4c816a064f93c7fe079d1265ebb0627b
hauntedcupoftea <andydchauhan@gmail.com>
2025-06-30 14:38:41 +0530

    enable terminal bell

--- home/terminals/kitty.nix
+++ home/terminals/kitty.nix
@@ -9,7 +9,7 @@
     };
     settings = {
       scrollback_lines = 10000;
-      enable_audio_bell = false;
+      enable_audio_bell = true;
       update_check_interval = 0;
       background_opacity = "0.95";
       shell = "${pkgs.fish}/bin/fish";
