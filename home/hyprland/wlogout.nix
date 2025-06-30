614ff31cc9b1325fb62b1352a56f104850fb2915
hauntedcupoftea <andydchauhan@gmail.com>
2025-06-30 14:51:20 +0530

    add wayland-logout to wlogout

--- home/hyprland/wlogout.nix
+++ home/hyprland/wlogout.nix
@@ -1,40 +1,9 @@
 { pkgs, ... }: {
+  home.packages = with pkgs; [ wayland-logout ];
+
   programs.wlogout = {
     enable = true;
     package = pkgs.wlogout;
-    # style = ''
-    #   window {
-    #     font-family: JetBrainMono;
-    #     font-size: 14px;
-    #     color: alpha(@color15, 0.7);
-    #     background-color: alpha(@color0, 0.9);
-    #   }
-
-    #   button {
-    #     background-repeat: no-repeat;
-    #     background-position: center;
-    #     background-size: 20%;
-    #     border: 1px solid alpha(@color11, 0.63);
-    #     border-radius: 6px;
-    #     margin: 5px;
-    #   }
-
-    #   button:focus {
-    #     border: 0px;
-    #   }
-
-    #   button:hover {
-    #     background-color: @color11;
-    #     color: #1e1e2e;
-    #     background-size: 30%;
-    #     transition: all 0.3s cubic-bezier(0.55, 0, 0.28, 1.682),
-    #       box-shadow 0.2s ease-in-out, background-color 0.2s ease-in-out;
-    #   }
-
-    #   button span {
-    #     font-size: 0.2em;
-    #   }
-    # '';
     layout = [
       {
         "label" = "lock";
@@ -56,7 +25,7 @@
       }
       {
         "label" = "logout";
-        "action" = "hyprctl dispatch exit";
+        "action" = "wayland-logout";
         "text" = "Logout";
         "keybind" = "e";
       }
