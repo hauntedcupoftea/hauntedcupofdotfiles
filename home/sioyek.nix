ad9aee61531544e4767c4419b887f187e099a006
hauntedcupoftea <andydchauhan@gmail.com>
2025-07-01 14:09:24 +0530

    sioyek still broken lol

--- home/sioyek.nix
+++ home/sioyek.nix
@@ -1,5 +1,15 @@
-{ ... }: {
+{ pkgs, ... }: {
   programs.sioyek = {
     enable = true;
+    # The package can also just be added to systemPackages
+    package = pkgs.symlinkJoin {
+      name = "sioyek";
+      paths = [ pkgs.sioyek ];
+      buildInputs = [ pkgs.makeWrapper ];
+      postBuild = ''
+        wrapProgram $out/bin/sioyek \
+          --set QT_QPA_PLATFORM xcb
+      '';
+    };
   };
 }
