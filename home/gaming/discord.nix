9b8a28af76e653f187c711bce1f24cbe880fb502
hauntedcupoftea <andydchauhan@gmail.com>
2025-07-15 18:19:56 +0530

    add old vesktop settings + nix flake update

--- home/gaming/discord.nix
+++ home/gaming/discord.nix
@@ -6,12 +6,19 @@
 
   programs.vesktop = {
     enable = true;
+    settings = {
+      arrpc = true;
+      hardwareAcceleration = true;
+    };
   };
 
   home.file = {
     "${config.xdg.configHome}/vesktop/themes" = {
       source = ../../custom-files/vesktop/themes;
       recursive = true;
+    };
+    "${config.xdg.configHome}/vesktop/settings/settings.json" = {
+      source = ../../custom-files/vesktop/settings.json;
     };
   };
 
