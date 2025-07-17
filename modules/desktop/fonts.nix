5122b45003c6e051e3f96b4539d8fba4c70631a4
hauntedcupoftea <andydchauhan@gmail.com>
2025-07-17 17:59:48 +0530

    add a bunch more fonts to hopefully fix text

--- modules/desktop/fonts.nix
+++ modules/desktop/fonts.nix
@@ -1,13 +1,15 @@
-{
-  config,
-  lib,
-  pkgs,
-  ...
+{ pkgs
+, ...
 }: {
   # Add nerdfonts
   fonts.packages = with pkgs; [
     nerd-fonts.fira-code
     nerd-fonts.droid-sans-mono
     nerd-fonts.jetbrains-mono
+    noto-fonts-emoji
+    noto-fonts-cjk-sans
+    noto-fonts-cjk-serif
+    material-symbols
+    material-icons
   ];
 }
