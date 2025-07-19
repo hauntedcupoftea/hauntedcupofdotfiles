84948e90e99f55855cc12d40292d80aeeb2ffcf3
hauntedcupoftea <andydchauhan@gmail.com>
2025-07-20 00:31:39 +0530

    add configuration for termfilechooser

--- home/xdg.nix
+++ home/xdg.nix
@@ -12,7 +12,16 @@
       };
       preferred = {
         default = [ "hyprland" ];
+        "org.freedesktop.impl.portal.FileChooser" = [ "termfilechooser" ];
       };
     };
   };
+
+  # Configure termfilechooser
+  home.file.".config/xdg-desktop-portal-termfilechooser/config".text = ''
+    [Default]
+    cmd=kitty yazi --chooser-file=%f
+    file=%f
+    directory=%d
+  '';
 }
