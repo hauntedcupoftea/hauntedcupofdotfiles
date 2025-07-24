5183e6ef04b01be9b7be79d0fe3ae9dc3ebbf0cd
hauntedcupoftea <andydchauhan@gmail.com>
2025-07-25 02:27:33 +0530

    actually use nh from flake

--- modules/core/nh.nix
+++ modules/core/nh.nix
@@ -1,4 +1,4 @@
-{ config, ... }:
+{ config, inputs, pkgs, ... }:
 let
   configurationLimit = toString config.boot.loader.grub.configurationLimit;
 in
@@ -6,6 +6,7 @@ in
   programs.nh = {
     enable = true;
     flake = "/home/tea/hauntedcupofdotfiles";
+    package = inputs.nh.packages.${pkgs.system}.default;
     clean = {
       enable = true;
       dates = "weekly";
