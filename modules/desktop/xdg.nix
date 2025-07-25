d0fe6c637a8ba431522c166a2bd9e4deb8147e3c
hauntedcupoftea <andydchauhan@gmail.com>
2025-07-25 17:09:53 +0530

    move xdg to nixos configuration

---
+++ modules/desktop/xdg.nix
@@ -0,0 +1,25 @@
+{ pkgs, ... }: {
+  xdg.portal = {
+    enable = true;
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-gtk
+      gnome-keyring
+      xdg-desktop-portal-termfilechooser
+    ];
+    config.common = {
+      default = [ "hyprland" "gtk" ];
+      "org.freedesktop.impl.portal.FileChooser" = [ "termfilechooser" "gtk" ];
+    };
+  };
+
+  home.file.".config/xdg-desktop-portal-termfilechooser/config".text = ''
+    [filechooser]
+    cmd=${../custom-files/termfilechooser/yazi-wrapper.sh}
+    env=TERMCMD=kitty
+        EDITOR=hx
+  '';
+
+  home.sessionVariables = {
+    GTK_USE_PORTAL = "1";
+  };
+}
