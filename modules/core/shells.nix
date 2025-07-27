582beb55a76b207ce93e0cc9124abe82370d5f05
hauntedcupoftea <andydchauhan@gmail.com>
2025-07-27 18:20:16 +0530

    fish undo

--- modules/core/shells.nix
+++ modules/core/shells.nix
@@ -4,7 +4,6 @@
   programs.fish = {
     enable = true;
     useBabelfish = true;
-    generateCompletions = false;
   };
   environment.systemPackages = [
     pkgs.kitty # required for the default Hyprland config
