3f09df54e45b6ad20596e08f040dd4680cd875e0
hauntedcupoftea <andydchauhan@gmail.com>
2025-07-27 14:42:09 +0530

    chore: flake update

--- home/dev/nvf.nix
+++ home/dev/nvf.nix
@@ -8,12 +8,6 @@
     enable = true;
     settings = {
       vim = {
-        # theme = {
-        #   enable = true;
-        #   name = "catppuccin";
-        #   style = "mocha";
-        # };
-
         statusline.lualine.enable = true;
         telescope.enable = true;
         autocomplete.nvim-cmp.enable = true;
