db16b1502b8aaaf1581927a8f193ebb161eb71ab
hauntedcupoftea <andydchauhan@gmail.com>
2025-07-27 18:18:49 +0530

    remove generateCompletions for pods.rs

--- modules/core/shells.nix
+++ modules/core/shells.nix
@@ -4,6 +4,7 @@
   programs.fish = {
     enable = true;
     useBabelfish = true;
+    generateCompletions = false;
   };
   environment.systemPackages = [
     pkgs.kitty # required for the default Hyprland config
