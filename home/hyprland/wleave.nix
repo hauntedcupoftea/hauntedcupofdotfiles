3f1e77f300f4d74e374c92852ce82e8e44bc5f9b
hauntedcupoftea <andydchauhan@gmail.com>
2025-07-29 19:39:07 +0530

    remove wleave icons def

--- home/hyprland/wleave.nix
+++ home/hyprland/wleave.nix
@@ -2,10 +2,6 @@
   home.packages = [ pkgs.wleave ];
 
   home.file = {
-    "${config.xdg.configHome}/wleave/icons" = {
-      source = ../../custom-files/wleave/icons;
-      recursive = true;
-    };
     "${config.xdg.configHome}/wleave/layout.json".text =
       # json
       ''
