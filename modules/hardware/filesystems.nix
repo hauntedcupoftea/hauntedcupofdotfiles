0d856c8034ba033775b1cc107e13c591d6506c10
hauntedcupoftea <andydchauhan@gmail.com>
2025-07-29 15:41:21 +0530

    remount games on my fs

--- modules/hardware/filesystems.nix
+++ modules/hardware/filesystems.nix
@@ -2,17 +2,17 @@
 }: {
   # Mount points for my Windows drives using the ntfs3 kernel driver
   fileSystems = {
-    # "/mnt/games" = {
-    #   device = "/dev/disk/by-uuid/01D807D412ACB800";
-    #   fsType = "ntfs3";
-    #   options = [
-    #     "rw"
-    #     "uid=1000"
-    #     "gid=100"
-    #     "umask=007"
-    #     "nofail"
-    #   ];
-    # };
+    "/mnt/games" = {
+      device = "/dev/disk/by-uuid/01D807D412ACB800";
+      fsType = "ntfs3";
+      options = [
+        "rw"
+        "uid=1000"
+        "gid=100"
+        "umask=007"
+        "nofail"
+      ];
+    };
     "/mnt/media" = {
       device = "/dev/disk/by-uuid/01DB740DE6A065F0";
       fsType = "ntfs3";
