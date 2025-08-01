29054a743916d61c4f5fbce08f3fb078b714d9f2
hauntedcupoftea <andydchauhan@gmail.com>
2025-08-01 16:40:08 +0530

    keep pushing the stone sisyphus you're almost there

--- modules/theme/stylix.nix
+++ modules/theme/stylix.nix
@@ -18,32 +18,52 @@ in
     polarity = currentPolarity;
     image = ../../wallpapers/malenia.jpg;
 
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/material-darker.yaml";
+    # base16Scheme = "${pkgs.base16-schemes}/share/themes/material-darker.yaml";
+    # override = with matugenTheme; {
+    #   base00 = background;
+    #   base01 = surface_container;
+    #   base02 = surface_bright;
+    #   base03 = outline;
+    #   base04 = secondary;
+    #   base05 = on_secondary_container;
+    #   # base06 = on_primary_container;
+    #   # base07 = on_primary_container;
+    #   # base08 = on_error_container;
+    #   # base09 = tertiary_fixed;
+    #   # base0A = primary_fixed;
+    #   # base0B = secondary_fixed;
+    #   # base0C = secondary;
+    #   # base0D = tertiary;
+    #   base0E = primary;
+    #   base0F = error_container;
+    # };
 
     # heavily modified but inspo from https://github.com/make-42/stylix/blob/matugen-clean-diff-rebuild/stylix/palette.nix
-    override = with matugenTheme; {
+    base16Scheme = with matugenTheme; {
       base00 = background;
       base01 = surface_container;
       base02 = surface_bright;
       base03 = outline;
       base04 = secondary;
       base05 = on_secondary_container;
-      # base06 = on_primary_container;
-      # base07 = on_primary_container;
-      # base08 = on_error_container;
-      # base09 = tertiary_fixed;
-      # base0A = primary_fixed;
-      # base0B = secondary_fixed;
-      # base0C = secondary;
-      # base0D = tertiary;
+      base06 = on_primary_container;
+      base07 = on_primary_container;
+      base08 = error_container;
+      base09 = tertiary_container;
+      base0A = tertiary;
+      base0B = secondary_container;
+      base0C = secondary;
+      base0D = primary_container;
       base0E = primary;
-      base0F = error_container;
+      base0F = on_error;
     };
+
     cursor = {
       name = "Bibata-Modern-Classic";
       package = pkgs.bibata-cursors;
       size = 36;
     };
+
     fonts = {
       sansSerif = {
         package = pkgs.nerd-fonts.fira-code;
@@ -60,6 +80,7 @@ in
         desktop = 14;
       };
     };
+
     opacity = {
       terminal = 0.85;
       desktop = 0.85;
