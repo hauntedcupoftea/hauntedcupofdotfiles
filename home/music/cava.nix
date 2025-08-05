9a7059c161290e4c89e4c58245f3ad324e7fa8bc
hauntedcupoftea <andydchauhan@gmail.com>
2025-08-05 20:04:24 +0530

    forgot stylix is on

--- home/music/cava.nix
+++ home/music/cava.nix
@@ -1,47 +1,50 @@
-{ matugenTheme, ... }: {
+{
+  # matugenTheme,
+  ...
+}: {
   programs.cava = {
     enable = true;
-    settings = {
-      general = {
-        framerate = 60;
-        autosens = true;
-        bars = 0;
-        bar_width = 1;
-        bar_spacing = 1;
-        center_align = true;
-      };
+    # settings = {
+    #   general = {
+    #     framerate = 60;
+    #     autosens = true;
+    #     bars = 0;
+    #     bar_width = 1;
+    #     bar_spacing = 1;
+    #     center_align = true;
+    #   };
 
-      input = {
-        method = "pipewire";
-        source = "auto";
-      };
+    #   input = {
+    #     method = "pipewire";
+    #     source = "auto";
+    #   };
 
-      output = {
-        method = "noncurses";
-        orientation = "bottom";
-        channels = "stereo";
-      };
+    #   output = {
+    #     method = "noncurses";
+    #     orientation = "bottom";
+    #     channels = "stereo";
+    #   };
 
-      color = {
-        background = "${matugenTheme.colors.dark.surface_container_lowest}";
-        gradient = true;
-        gradientColors = [
-          "${matugenTheme.colors.dark.primary}"
-          "${matugenTheme.colors.dark.secondary}"
-          "${matugenTheme.colors.dark.tertiary}"
-          "${matugenTheme.colors.dark.primary_container}"
-          "${matugenTheme.colors.dark.secondary_container}"
-          "${matugenTheme.colors.dark.tertiary_container}"
-          "${matugenTheme.colors.dark.on_primary_container}"
-          "${matugenTheme.colors.dark.on_secondary_container}"
-        ];
-      };
+    #   color = {
+    #     background = "${matugenTheme.colors.dark.surface_container_lowest}";
+    #     gradient = true;
+    #     gradientColors = [
+    #       "${matugenTheme.colors.dark.primary}"
+    #       "${matugenTheme.colors.dark.secondary}"
+    #       "${matugenTheme.colors.dark.tertiary}"
+    #       "${matugenTheme.colors.dark.primary_container}"
+    #       "${matugenTheme.colors.dark.secondary_container}"
+    #       "${matugenTheme.colors.dark.tertiary_container}"
+    #       "${matugenTheme.colors.dark.on_primary_container}"
+    #       "${matugenTheme.colors.dark.on_secondary_container}"
+    #     ];
+    #   };
 
-      smoothing = {
-        monstercat = true;
-        waves = true;
-        noise_reduction = 85;
-      };
-    };
+    #   smoothing = {
+    #     monstercat = true;
+    #     waves = true;
+    #     noise_reduction = 85;
+    #   };
+    # };
   };
 }
