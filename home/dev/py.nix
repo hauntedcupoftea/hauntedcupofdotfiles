2ac3d06d4a69379a0ca04a28401b41987f6c8557
hauntedcupoftea <andydchauhan@gmail.com>
2025-08-15 18:13:15 +0530

    change to basedpyright

--- home/dev/py.nix
+++ home/dev/py.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }: {
   home.packages = with pkgs; [
     uv # package manager
-    pyright # Microsoft's Pyright LSP
+    basedpyright # Microsoft's Pyright LSP
     ruff # Code linter + formatter
   ];
 }
