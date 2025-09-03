41e695f92ce789c94b9e563fdeb99e33ac9bbdb4
hauntedcupoftea <andydchauhan@gmail.com>
2025-09-03 16:58:56 +0530

    update ssh config to new format

--- home/security.nix
+++ home/security.nix
@@ -1,15 +1,26 @@
-{ pkgs, ... }: {
+{pkgs, ...}: {
   home.packages = [
     pkgs.seahorse
   ];
 
   services.gnome-keyring = {
     enable = true;
-    components = [ "pkcs11" "secrets" ];
+    components = ["pkcs11" "secrets"];
   };
 
   programs.ssh = {
     enable = true;
-    addKeysToAgent = "yes";
+    matchBlocks."*" = {
+      forwardAgent = false;
+      addKeysToAgent = "no";
+      compression = false;
+      serverAliveInterval = 0;
+      serverAliveCountMax = 3;
+      hashKnownHosts = false;
+      userKnownHostsFile = "~/.ssh/known_hosts";
+      controlMaster = "no";
+      controlPath = "~/.ssh/master-%r@%n:%p";
+      controlPersist = "no";
+    };
   };
 }
