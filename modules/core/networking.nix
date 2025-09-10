e22b55724d6bb5df5f1b86d2fe77404abbaef153
hauntedcupoftea <andydchauhan@gmail.com>
2025-09-10 19:43:02 +0530

    add dhcp here

--- modules/core/networking.nix
+++ modules/core/networking.nix
@@ -1,7 +1,13 @@
-{ ...
-}: {
+{lib, ...}: {
   # Enable networking
   networking = {
+    # Enables DHCP on each Ethernet and wireless interface. In case of scripted networking
+    # (the default) this is the recommended approach. When using systemd-networkd it's
+    # still possible to use this option, but it's recommended to use it in conjunction
+    # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
+    useDHCP = lib.mkDefault true;
+    # networking.interfaces.enp5s0.useDHCP = lib.mkDefault true;
+    # networking.interfaces.wlo1.useDHCP = lib.mkDefault true;
     networkmanager = {
       enable = true;
       wifi.backend = "iwd";
