b6bb6625c7ce4c0981e3f70f5baa8567c14d4d46
hauntedcupoftea <andydchauhan@gmail.com>
2025-10-12 00:05:08 +0530

    try to fix dungeondraft exports

--- pkgs/dungeondraft.nix
+++ pkgs/dungeondraft.nix
@@ -12,9 +12,10 @@
   alsa-lib,
   udev,
   zenity,
+  xdg-user-dirs,
 }:
 stdenv.mkDerivation rec {
-  pname = "Dungeondraft";
+  pname = "dungeondraft";
   version = "1.2.0.1";
 
   src = requireFile {
@@ -22,6 +23,7 @@ stdenv.mkDerivation rec {
     url = "https://dungeondraft.net/";
     hash = "sha256-UvvUCQ1RkhwBPMet/zD0JjI7DPbF4ixzOX85Fi3v/BE=";
   };
+
   sourceRoot = ".";
   unpackCmd = "${dpkg}/bin/dpkg-deb -x $curSrc .";
 
@@ -32,6 +34,7 @@ stdenv.mkDerivation rec {
     autoPatchelfHook
     makeWrapper
   ];
+
   buildInputs = [
     libGL
     libkrb5
@@ -45,23 +48,41 @@ stdenv.mkDerivation rec {
   ];
 
   installPhase = ''
-    runHook preInstall
-    mkdir -p $out/bin
-    cp -R usr/share opt $out/
-    mkdir -p $out/share/icons/hicolor/256x256/apps
-    mv $out/opt/Dungeondraft/Dungeondraft.png $out/share/icons/hicolor/256x256/apps/Dungeondraft.png
-    ln -s $out/opt/Dungeondraft/Dungeondraft.x86_64 $out/bin/Dungeondraft.x86_64
-    substituteInPlace \
-      $out/share/applications/Dungeondraft.desktop \
-        --replace-warn /opt/Dungeondraft/ "" \
-        --replace-warn Dungeondraft.png Dungeondraft \
-        --replace-warn /opt/Dungeondraft $out/opt/Dungeondraft
-    runHook postInstall
+        runHook preInstall
+
+        # Create necessary directories
+        mkdir -p $out/bin
+        mkdir -p $out/share/applications
+        mkdir -p $out/share/icons/hicolor/256x256/apps
+
+        # Copy application files
+        cp -R opt/Dungeondraft $out/opt/
+
+        # Copy icon to proper location
+        cp $out/opt/Dungeondraft/Dungeondraft.png $out/share/icons/hicolor/256x256/apps/Dungeondraft.png
+
+        # Create wrapper script with proper PATH
+        makeWrapper $out/opt/Dungeondraft/Dungeondraft.x86_64 $out/bin/dungeondraft \
+          --prefix PATH : ${lib.makeBinPath [zenity xdg-user-dirs]} \
+          --chdir $out/opt/Dungeondraft
+
+        # Create desktop entry that uses the wrapper
+        cat > $out/share/applications/dungeondraft.desktop <<EOF
+    [Desktop Entry]
+    Version=1.0
+    Type=Application
+    Name=Dungeondraft
+    Comment=Mapmaking tool for Tabletop Roleplaying Games
+    Exec=$out/bin/dungeondraft
+    Icon=Dungeondraft
+    Terminal=false
+    Categories=Graphics;2DGraphics;
+    StartupWMClass=Dungeondraft
+    EOF
+
+        runHook postInstall
   '';
-  postInstall = ''
-    wrapProgram $out/bin/Dungeondraft.x86_64 \
-      --prefix PATH : ${lib.makeBinPath [zenity]}
-  '';
+
   postFixup = ''
     patchelf \
       --add-needed ${udev}/lib/libudev.so.1 \
@@ -69,12 +90,12 @@ stdenv.mkDerivation rec {
       $out/opt/Dungeondraft/Dungeondraft.x86_64
   '';
 
-  meta = {
+  meta = with lib; {
     homepage = "https://dungeondraft.net/";
     description = "Mapmaking tool for Tabletop Roleplaying Games, designed for battlemap scale";
-    license = lib.licenses.unfree;
+    license = licenses.unfree;
     platforms = ["x86_64-linux"];
-    sourceProvenance = with lib.sourceTypes; [binaryNativeCode];
-    mainProgram = "Dungeondraft.x86_64";
+    sourceProvenance = with sourceTypes; [binaryNativeCode];
+    mainProgram = "dungeondraft";
   };
 }
