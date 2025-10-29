ef0048bf3c9b9d625dee0390480b65167a3b3d3f
hauntedcupoftea <andydchauhan@gmail.com>
2025-10-29 17:40:07 +0530

    remove redundant package addition error

--- modules/core/shells.nix
+++ modules/core/shells.nix
@@ -1,11 +1,6 @@
-{ pkgs
-, ...
-}: {
+{...}: {
   programs.fish = {
     enable = true;
     useBabelfish = true;
   };
-  environment.systemPackages = [
-    pkgs.kitty # required for the default Hyprland config
-  ];
 }
