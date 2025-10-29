e87419bc9359b2d26de81bd7e3d34692823082b1
hauntedcupoftea <andydchauhan@gmail.com>
2025-10-29 17:38:27 +0530

    remove documentation option because android cri

--- hosts/ge66-raider/default.nix
+++ hosts/ge66-raider/default.nix
@@ -21,6 +21,7 @@
 
   # Host-specific overrides can go here
   networking.hostName = "Anand-GE66-Raider";
+  documentation.nixos.enable = false;
 
   # This value determines the NixOS release
   system.stateVersion = "24.11";
