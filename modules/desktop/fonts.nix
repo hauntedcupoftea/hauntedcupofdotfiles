d1e5ddc77f3054771f4db8f74946fe27dbd4d45a
hauntedcupoftea <andydchauhan@gmail.com>
2025-11-02 16:00:35 +0530

    font rename to fix build error

--- modules/desktop/fonts.nix
+++ modules/desktop/fonts.nix
@@ -1,12 +1,10 @@
-{ pkgs
-, ...
-}: {
+{pkgs, ...}: {
   # Add nerdfonts
   fonts.packages = with pkgs; [
     nerd-fonts.fira-code
     nerd-fonts.droid-sans-mono
     nerd-fonts.jetbrains-mono
-    noto-fonts-emoji
+    noto-fonts-color-emoji
     noto-fonts-cjk-sans
     noto-fonts-cjk-serif
     material-symbols
