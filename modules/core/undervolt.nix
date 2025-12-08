60b0be243b86890a2965796403e031a92ee70903
hauntedcupoftea <andydchauhan@gmail.com>
2025-12-08 14:13:34 +0530

    remove undervolt because something is broke

--- modules/core/undervolt.nix
+++ modules/core/undervolt.nix
@@ -1,11 +1,11 @@
 {...}: {
-  services.undervolt = {
-    enable = true;
-    coreOffset = -45;
-    gpuOffset = -25;
-    uncoreOffset = -25;
-    analogioOffset = 0;
-  };
+  # services.undervolt = {
+  #   enable = true;
+  #   coreOffset = -45;
+  #   gpuOffset = -25;
+  #   uncoreOffset = -25;
+  #   analogioOffset = 0;
+  # };
 
   programs.tuxclocker = {
     enable = true;
