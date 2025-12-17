09531a1fa8b585068517470a146ab75daa8c6564
hauntedcupoftea <andydchauhan@gmail.com>
2025-12-17 18:44:14 +0530

    new novideo, gale not back yet

--- modules/hardware/nvidia.nix
+++ modules/hardware/nvidia.nix
@@ -22,7 +22,7 @@
 
     nvidiaSettings = true;
 
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    package = config.boot.kernelPackages.nvidiaPackages.beta;
   };
 
   environment.systemPackages = with pkgs; [nvidia-vaapi-driver libva-utils];
