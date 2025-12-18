365423114cc1c81b3e1b7989abec914236e80224
hauntedcupoftea <andydchauhan@gmail.com>
2025-12-18 17:17:03 +0530

    all aboard rmpc

--- home/music/mpd.nix
+++ home/music/mpd.nix
@@ -1,7 +1,13 @@
-{pkgs, ...}: {
-  home.packages = with pkgs; [
-    rmpc
-  ];
+{...}: {
+  programs.rmpc = {
+    enable = true;
+    config = ''
+      (
+        max_fps: 60,
+        enable_mouse: true,
+      )
+    '';
+  };
 
   services = {
     mpd = {
@@ -12,6 +18,20 @@
     };
     mpd-mpris = {
       enable = true;
+    };
+  };
+
+  xdg.desktopEntries.rmpc = {
+    name = "rmpc";
+    genericName = "MPD Client";
+    comment = "A modern, configurable terminal MPD client";
+    exec = "rmpc";
+    icon = "multimedia-audio-player";
+    terminal = true;
+    type = "Application";
+    categories = ["Audio" "AudioVideo" "Music" "Player" "ConsoleOnly"];
+    settings = {
+      Keywords = "music;mpd;player;audio;tui;";
     };
   };
 }
