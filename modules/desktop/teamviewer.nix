92d0387a94d58ab74d8a094d174e7f8af92ab0f9
hauntedcupoftea <andydchauhan@gmail.com>
2025-12-29 15:57:08 +0530

    fix teamviewer conflict

--- modules/desktop/teamviewer.nix
+++ modules/desktop/teamviewer.nix
@@ -1,3 +1,12 @@
-{ ... }: {
+{pkgs, ...}: {
   services.teamviewer.enable = true;
+
+  services.teamviewer.package = pkgs.teamviewer.overrideAttrs (old: {
+    postFixup =
+      (old.postFixup or "")
+      + ''
+        wrapProgram $out/bin/teamviewer \
+          --unset QT_STYLE_OVERRIDE
+      '';
+  });
 }
