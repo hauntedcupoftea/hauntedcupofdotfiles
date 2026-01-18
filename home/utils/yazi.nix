9f371c27f976f83d0aa0c04ee5683f970b589ee7
hauntedcupoftea <andydchauhan@gmail.com>
2026-01-19 00:02:12 +0530

    modules/home: redo yazi config

--- home/utils/yazi.nix
+++ home/utils/yazi.nix
@@ -10,16 +10,35 @@
     imagemagick
     dragon-drop
     trash-cli
+    ouch
+    mediainfo
+    imagemagick
   ];
 
   programs.yazi = {
     enable = true;
     enableFishIntegration = true;
     plugins = {
+      "full-border" = pkgs.yaziPlugins.full-border;
+      "git" = pkgs.yaziPlugins.git;
+      # "gitui" = pkgs.yaziPlugins.gitui; # doesn't work
+      "mediainfo" = pkgs.yaziPlugins.mediainfo;
+      "mount" = pkgs.yaziPlugins.mount;
+      "ouch" = pkgs.yaziPlugins.ouch;
       "recycle-bin" = pkgs.yaziPlugins.recycle-bin;
+      "starship" = pkgs.yaziPlugins.starship;
+      # "wl-clipboard" = pkgs.yaziPlugins.wl-clipboard; # doesn't work
     };
     initLua = ''
+      require("full-border"):setup()
       require("recycle-bin"):setup()
+      require("starship"):setup()
+      th.git = th.git or {}
+      th.git.modified = ui.Style():fg("blue")
+      th.git.deleted = ui.Style():fg("red"):bold()
+      require("git"):setup {
+        type = ui.Border.ROUNDED,
+      }
     '';
     settings = {
       mgr = {
@@ -34,6 +53,52 @@
           }
         ];
       };
+      plugin.prepend_fetcher = [
+        {
+          id = "git";
+          url = "*";
+          run = "git";
+        }
+        {
+          id = "git";
+          url = "*/";
+          run = "git";
+        }
+      ];
+      plugin = {
+        prepend_previewers = [
+          {
+            mime = "application/{*zip,tar,bzip2,7z*,rar,xz,zstd,java-archive}";
+            run = "ouch";
+          }
+          {
+            mime = "{audio,video,image}/*";
+            run = "mediainfo";
+          }
+          {
+            mime = "application/subrip";
+            run = "mediainfo";
+          }
+          {
+            mime = "application/postscript";
+            run = "mediainfo";
+          }
+        ];
+        prepend_preloaders = [
+          {
+            mime = "{audio,video,image}/*";
+            run = "mediainfo";
+          }
+          {
+            mime = "application/subrip";
+            run = "mediainfo";
+          }
+          {
+            mime = "application/postscript";
+            run = "mediainfo";
+          }
+        ];
+      };
     };
     keymap = {
       mgr.prepend_keymap = [
@@ -43,13 +108,29 @@
           desc = "Drag and drop selected files";
         }
         {
+          on = "<C-y>";
+          run = ["plugin wl-clipboard"];
+        }
+        {
           on = ["R" "b"];
           run = ''plugin recycle-bin'';
           desc = "Open recycle bin";
         }
+        {
+          on = "M";
+          run = "plugin mount";
+        }
+        {
+          on = ["g" "i"];
+          run = "plugin gitui";
+          desc = "run gitui";
+        }
+        {
+          on = ["C"];
+          run = "plugin ouch";
+          desc = "Compress with ouch";
+        }
       ];
     };
   };
-
-  # catppuccin.yazi.enable = true;
 }
