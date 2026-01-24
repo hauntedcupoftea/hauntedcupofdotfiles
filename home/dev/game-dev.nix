f50f1301efb2457a2f956f553bb8465f7ee42256
hauntedcupoftea <andydchauhan@gmail.com>
2026-01-25 01:11:42 +0530

    home/dev: freecad is fix!

--- home/dev/game-dev.nix
+++ home/dev/game-dev.nix
@@ -1,21 +1,20 @@
 {pkgs, ...}: let
   # https://github.com/NixOS/nixpkgs/issues/468456
-  # https://nixpkgs-tracker.ocfox.me/?pr=480871
-  # freecadWayland = pkgs.symlinkJoin {
-  #   name = "freecad-wayland-fix";
-  #   paths = [
-  #     pkgs.freecad-wayland
-  #   ];
-  #   buildInputs = [pkgs.makeWrapper];
-  #   postBuild = ''
-  #     wrapProgram $out/bin/FreeCAD \
-  #       --prefix MESA_LOADER_DRIVER_OVERRIDE : zink \
-  #       --prefix __EGL_VENDOR_LIBRARY_FILENAMES : ${pkgs.mesa}/share/glvnd/egl_vendor.d/50_mesa.json \
-  #   '';
-  # };
+  freecadWayland = pkgs.symlinkJoin {
+    name = "freecad-wayland-fix";
+    paths = [
+      pkgs.freecad-wayland
+    ];
+    buildInputs = [pkgs.makeWrapper];
+    postBuild = ''
+      wrapProgram $out/bin/FreeCAD \
+        --prefix MESA_LOADER_DRIVER_OVERRIDE : zink \
+        --prefix __EGL_VENDOR_LIBRARY_FILENAMES : ${pkgs.mesa}/share/glvnd/egl_vendor.d/50_mesa.json \
+    '';
+  };
 in {
   home.packages = with pkgs; [
-    # freecadWayland
+    freecadWayland
     godot
   ];
 }
