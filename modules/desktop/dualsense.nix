8f8fde96e7abeaa35f11a1c2308220c8cfc39e3e
hauntedcupoftea <andydchauhan@gmail.com>
2026-02-07 11:51:08 +0530

    dualsense haptics fix for alsa

--- modules/desktop/dualsense.nix
+++ modules/desktop/dualsense.nix
@@ -3,4 +3,16 @@
     # Ignore DualSense Touchpad as a mouse
     ACTION=="add|change", KERNEL=="event[0-9]*", ATTRS{name}=="*DualSense Wireless Controller Touchpad", ENV{LIBINPUT_IGNORE_DEVICE}="1"
   '';
+
+  nixpkgs.overlays = [
+    (final: prev: {
+      alsa-ucm-conf = prev.alsa-ucm-conf.overrideAttrs (oldAttrs: {
+        patches =
+          (oldAttrs.patches or [])
+          ++ [
+            ../../custom-files/patches/dualsense-alsa-fix.patch
+          ];
+      });
+    })
+  ];
 }
