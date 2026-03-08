142614540da62f96f2b6e1b04126f56c5a397b61
hauntedcupoftea <andydchauhan@gmail.com>
2026-03-08 16:00:40 +0530

    core: i think this fixed the sessionEntries

--- modules/core/default.nix
+++ modules/core/default.nix
@@ -6,9 +6,12 @@
     ./nh.nix
     ./locale.nix
     ./location.nix
+    # the infamous greeter war (everything changed when the greetd nation attacked)
+    # ./cosmic-greeter.nix
     ./kurukurudm.nix
     # ./sddm.nix
     # ./greetd.nix
+
     ./ssh.nix
     ./shells.nix
     ./security.nix
--- modules/core/greetd.nix
+++ modules/core/greetd.nix
@@ -1,4 +1,20 @@
-{ ... }: {
-  services.greetd.enable = true;
-  programs.regreet.enable = true;
+{
+  lib,
+  pkgs,
+  config,
+  ...
+}: {
+  services.greetd = {
+    enable = true;
+    greeterManagesPlymouth = true;
+    settings = {
+      default_session = {
+        command = "${lib.getExe pkgs.tuigreet} --time --cmd start-hyprland --sessions /run/current-system/sw/share/wayland-sessions";
+      };
+    };
+  };
+
+  boot.kernelParams =
+    map (m: "video=${m.name}:${m.resolution}@${toString m.refreshRate}")
+    config.dotfiles.desktop.monitors;
 }
