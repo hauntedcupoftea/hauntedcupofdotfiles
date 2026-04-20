8b2a58e31914c43076874ac83da063b78b9be73c
hauntedcupoftea <andydchauhan@gmail.com>
2026-04-20 18:32:04 +0530

    chore: update

--- modules/desktop/mail.nix
+++ modules/desktop/mail.nix
@@ -1,3 +1,11 @@
-{...}: {
-  programs.thunderbird.enable = true;
+{pkgs, ...}: {
+  programs.thunderbird = {
+    enable = true;
+    package =
+      (import pkgs.path {
+        inherit (pkgs) system;
+        config.allowUnfree = true;
+        # cudaSupport intentionally omitted
+      }).thunderbird;
+  };
 }
