7c24c39d24db41d41ab339ae58b813230d7fb053
hauntedcupoftea <andydchauhan@gmail.com>
2026-07-14 13:47:34 +0530

    ge66: fix monitors

--- hosts/ge66-raider/default.nix
+++ hosts/ge66-raider/default.nix
@@ -19,22 +19,14 @@
       audio.enable = true;
       monitors = [
         {
-          name = "DP-2";
-          resolution = "2560x1440";
-          refreshRate = 165;
-          position = "0x0";
-          scale = 1;
-          vrr = true;
-          primary = true;
-        }
-        {
           name = "eDP-1";
           resolution = "1920x1080";
           refreshRate = 240;
-          position = "2560x360";
+          position = "0";
+          hdr = true;
           scale = 1;
           vrr = false;
-          primary = false;
+          primary = true;
         }
       ];
     };
