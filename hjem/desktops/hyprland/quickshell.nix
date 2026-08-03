9a46e32601118c5ccf3d5dfa4e4fcd886ef09afe
hauntedcupoftea <andydchauhan@gmail.com>
2026-08-03 18:21:20 +0530

    fix: potential systemd-cycle and slow start

--- hjem/desktops/hyprland/quickshell.nix
+++ hjem/desktops/hyprland/quickshell.nix
@@ -43,7 +43,7 @@ in {
       description = "QuickShell UI service";
       wantedBy = [cfg.systemdTarget];
       partOf = [cfg.systemdTarget];
-      after = [cfg.systemdTarget];
+      # after = [cfg.systemdTarget];
 
       serviceConfig = {
         ExecStart = ''
