9a8cea96f4ccf2a197fc1b3de9bbcbcc7940d4c2
hauntedcupoftea <andydchauhan@gmail.com>
2026-08-20 22:52:16 +0530

    fix: disable tuigreet/greetd for now

--- modules/core/default.nix
+++ modules/core/default.nix
@@ -7,10 +7,10 @@
     ./locale.nix
     ./location.nix
     # the infamous greeter war (everything changed when the greetd nation attacked)
-    # ./cosmic-greeter.nix
+    ./cosmic-greeter.nix
     # ./kurukurudm.nix
     # ./sddm.nix
-    ./greetd.nix
+    # ./greetd.nix
 
     ./ssh.nix
     ./shells.nix
